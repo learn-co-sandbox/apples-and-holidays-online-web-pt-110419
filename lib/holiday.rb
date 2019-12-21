4bd92369785c10710f0f53bed765d8e8d239daca
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-21 05:21:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
     holiday_hash.each do |season, holiday|
       puts "#{season.capitalize}:"
-      holiday_hash.each do |
+      holiday.each do |
 end
 
 def all_holidays_with_bbq(holiday_hash)
